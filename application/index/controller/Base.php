634da518325478120d63e326673878228949dfef
梁泽祥 <914057262@qq.com>
2018-10-12 20:46:20 +0800

    模版分离

---
+++ application/index/controller/Base.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jon
+ * Date: 2018/10/12
+ * Time: 下午5:30
+ */
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Base extends Controller
+{
+
+}
